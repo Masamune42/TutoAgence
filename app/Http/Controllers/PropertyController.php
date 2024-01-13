f9a2847eb57064c322b4ff56f0188a422032a6b5
Alex <a.caignard@hotmail.fr>
2024-01-13 18:57:45 +0100

    Listing

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -10,7 +10,7 @@ class PropertyController extends Controller
 {
     public function index(SearchPropertiesRequest $request)
     {
-        $query = Property::query();
+        $query = Property::query()->orderBy('created_at', 'desc');
         if($request->validated('price')) {
             $query = $query->where('price', '<=', $request->validated('price'));
         }
