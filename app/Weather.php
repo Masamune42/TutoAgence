e5f931f14dafd33580b75d8b97da616593ef1ef7
Alexandre Caignard <38729394+Masamune42@users.noreply.github.com>
2024-03-13 22:18:58 +0100

    Correction : commentaires sur la partie des tests

--- app/Weather.php
+++ app/Weather.php
@@ -8,20 +8,22 @@ use Illuminate\Support\Facades\Cache;
 
 class Weather
 {
+    // Partie test avec Laravel
     // Aproche 2 : Utilisation du constructeur avec le Repository
-    public function __construct(private Repository $cache)
-    {
-    }
+//    public function __construct(private Repository $cache)
+//    {
+//    }
 
     public function isSunnyTomorrow()
     {
+        // Partie test avec Laravel
         // Approche 1 : utilisation de Facade dans les services
 //        $result = Cache::get('weather');
         // Aproche 2 : Utilisation du constructeur avec le Repository
-        $result = $this->cache->get('weather');
-        if($result !== null) {
-            return $result;
-        }
+//        $result = $this->cache->get('weather');
+//        if($result !== null) {
+//            return $result;
+//        }
 //        ...
         return true;
     }
